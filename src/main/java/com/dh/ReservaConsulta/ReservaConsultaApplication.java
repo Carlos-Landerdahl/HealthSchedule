ea93636fe13b56a9427aad1a76a40970f257c1c4
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-05-31 21:34:51 -0300

    Start: First commit

---
+++ src/main/java/com/dh/ReservaConsulta/ReservaConsultaApplication.java
@@ -0,0 +1,13 @@
+package com.dh.ReservaConsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaConsultaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/ReservaConsulta/ReservaConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.ReservaConsulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
